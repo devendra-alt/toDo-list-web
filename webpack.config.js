f67f7c4a0a1033c787aaedb52c940249c07bc679
devendra-alt <devendramulewa171@gmail.com>
2023-06-27 16:18:35 +0530

    Added webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+module.exports = {
+  mode: 'development',
+  entry: { bundle: path.resolve(__dirname, 'src/index.js') },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
+    assetModuleFilename: '[name][ext]',
+    clean: true,
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(svg|png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'ToDo List',
+      filename: 'index.html',
+      template: 'src/index.html',
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
+};
