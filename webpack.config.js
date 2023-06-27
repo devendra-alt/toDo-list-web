d750463d2d8466d3edf2b4a983425a5bbee18b9f
devendra-alt <devendramulewa171@gmail.com>
2023-06-27 17:42:24 +0530

    Added babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,16 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
         test: /\.(svg|png|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
