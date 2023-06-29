980d3f87d41de054da1ea75633ef6a755c8042fd
devendra-alt <devendramulewa171@gmail.com>
2023-06-29 15:06:40 +0530

    Added dom.js

---
+++ tests/dom.js
@@ -0,0 +1,10 @@
+const dom = `
+<article id="todoApp">
+      <h1>Today's To Do</h1>
+      <input id="add-task-input" placeholder="Add to your list..." />
+      <ul id="todo-list"></ul>
+      <button id="clear-completed-btn">Clear all completed</button>
+</article>
+`;
+
+export default dom;
